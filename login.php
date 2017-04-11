c4dc3957bff5777a46874b695d9c7949a7966a44
Peter <1408463@rgu.ac.uk>
2017-04-11 14:06:43 +0100

    login form bg

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ $password = stripslashes($password);
 $studentid = $conn->real_escape_string($studentid);
 $password = $conn->real_escape_string($password);
 //$sql="SELECT * FROM $tbl_name WHERE student_id='$studentid' and pword='$password'";
-$query = $conn->query("SELECT student_id, pword FROM users WHERE student_id='$studentid'");
+$query = $conn->query("SELECT student_id, password FROM users WHERE student_id='$studentid'");
 $row=$query->fetch_array();
 
 // Mysql_num_row is counting table row
@@ -34,6 +34,6 @@ if (password_verify($password, $row['password']) && $count==1) {
      <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Invalid Username or Password !
     ";
 
-} 
+}
 $conn->close();
 ?>

36a6f058cc0c05eda088b848147340b73ad94116
peterwronski <1408463@rgu.ac.uk>
2017-04-12 15:40:03 +0100

    debug

--- login_success.php
+++ login_success.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
+include('header.php');
 if (isset($_SESSION['userSession'])&&(($_SESSION['userSession']) == true)){
-    include('header.php');
+
     echo'<h1>LOGGED IN AS '.$_SESSION['userSession'].'</h1>';
 }
 ?>

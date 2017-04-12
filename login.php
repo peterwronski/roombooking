76b0b77802a5704d2a6d3e57c7ebce4abfd003e7
peterwronski <1408463@rgu.ac.uk>
2017-04-12 14:42:31 +0100

    debug

--- login.php
+++ login.php
@@ -13,10 +13,10 @@ echo "Login echo: " .$studentid .$password;
 echo '<br/> Host:' .$connectstr_dbhost .'<br/>Dbase: ' .$connectstr_dbname .'<br/>Name: ' .$connectstr_dbusername;
 
 // To protect MySQL injection (more detail about MySQL injection)
-$studentid = stripslashes($studentid);
-$password = stripslashes($password);
-$studentid = $conn->real_escape_string($studentid);
-$password = $conn->real_escape_string($password);
+// $studentid = stripslashes($studentid);
+// $password = stripslashes($password);
+// $studentid = $conn->real_escape_string($studentid);
+// $password = $conn->real_escape_string($password);
 
 
 //$sql="SELECT * FROM $tbl_name WHERE student_id='$studentid' and pword='$password'";

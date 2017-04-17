70a07f8232895e217d96516fad2a12e792f5d038
peterwronski <1408463@rgu.ac.uk>
2017-04-17 21:50:07 +0100

    debug

--- index.php
+++ index.php
@@ -25,13 +25,13 @@ include 'header.php';
             Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.
             Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.
             Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-            Donec non enim in turpis pulvinar facilisis. Ut felis. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            Donec non enim in turpis pulvinar facilisis. Utt felis. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
             It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
     </div>
 
-        <div class="col-lg-4 contentbox"><img src="img/building.jpg" class="img-responsive"/></div>
+        <div class="col-lg-4 contentbox"><img src="img/building.jpg" class="img-responsive" /></div>
 
         <div class="col-lg-4 contentbox"><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
                 Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form,

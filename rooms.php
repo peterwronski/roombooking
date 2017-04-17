92bbe62b8b6b756d7ea84a56d9d4b05e2aadb187
peterwronski <1408463@rgu.ac.uk>
2017-04-17 02:20:12 +0100

    rooms

--- rooms.php
+++ rooms.php
@@ -37,5 +37,23 @@ echo '</table>
     </div>
 </div>';
 
+if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin'){
+    echo'
+    <form action="addroom.php" method="POST">
+                   <label><b>Room Name</b></label>
+                   <input type="text" name="roomname" placeholder="Room Name id="roomname" required/>
+<br/>
+                   <label><b>Room Size</b></label>
+                   <input type="text" name="roomsize" id="roomsize" placeholder="Room size" required/>
+                   <br/>
+                   <label><b>Room Description</b></label>
+                   <textarea name="roomdesc" id="roomdesc" placeholder="Enter a short description of the room, i.e. number of desks, computers, etc."></textarea>
+                   
+                   <input type="submit" name="addroom-submit"/>
+
+               </form>';
+
+}
+
 $conn->close();
 ?>

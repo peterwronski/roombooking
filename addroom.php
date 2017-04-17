bdc2c19b5fe1deb19563be6fc141926c3e4f24fd
peterwronski <1408463@rgu.ac.uk>
2017-04-17 02:36:48 +0100

    rooms

--- addroom.php
+++ addroom.php
@@ -1,1 +1,9 @@
 <?php
+if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin'){
+    $roomname = $_POST["roomname"];
+    $roomsize = $_POST["roomnsize"];
+    $roomdesc = $_POST["roomdesc"];
+}
+    $query = $conn->query("INSERT INTO room VALUES (' $roomname .',' .$roomsize .',' .roomdesc .')";
+};
+?>

4bcdff4ed54477b4afde430d9d708a8c8565793b
peterwronski <1408463@rgu.ac.uk>
2017-04-17 22:39:19 +0100

    debug

--- addbooking.php
+++ addbooking.php
@@ -4,16 +4,29 @@ if (isset($_SESSION['userloggedin'])&&(($_SESSION['userloggedin']) == true)) {
     include('dbconnect.php');
 
     $query = $conn->query("SELECT room_name, room_id FROM room");
-echo '<form method="POST">
-        <label>Room name</label>
-        <select>';
+echo '
+<table class="loginform"></table>
+        <form method="POST">
+        <tr><td> <label>Room name</label></td> <td><select>
+           
+                ';
     while($row = $query->fetch_assoc()) {
 
 
-      echo'<option value='.$row["room_id"].'>' .$row["room_name"] .'</option>';
+                    echo'<option value='.$row["room_id"].'>' .$row["room_name"] .'</option>';
     };
-     echo'</select>
-        </form>';
+     echo'</select></td></tr>
+
+        <tr><td class="loginform"><label>Booking date</label></td><td class="loginform"><input type="date" required></input></td></tr>
+             
+        <tr><td class="loginform"><label>Booking time</label></td><td class="loginform"><input type="time" required></input></td></tr>     
+                
+        
+            
+                
+        </form>
+        
+        ';
 }
 
 //$row=$query->fetch_array();

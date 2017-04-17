373c876392f4348f0f74c1b1ec872be3d2f97331
peterwronski <1408463@rgu.ac.uk>
2017-04-17 03:25:08 +0100

    rooms

--- addroom.php
+++ addroom.php
@@ -10,5 +10,14 @@ if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin'){
     $roomdesc = $conn->real_escape_string($roomdesc);
 
     $query = $conn->query("INSERT INTO room VALUES ('$roomname ','$roomsize','$roomdesc')");
-};
+    header("Location:index.php#rooms");
+}
+else{
+    ?>
+<script type="text/javascript">
+    alert("You ain't supposed to be here son.");
+
+    window.location = "index.php"
+</script>
+<?php }
 ?>

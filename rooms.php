3d428d5f6ab0bec85d92c0d99a9f757aa23e9772
peterwronski <1408463@rgu.ac.uk>
2017-04-17 01:14:46 +0100

    rooms

--- rooms.php
+++ rooms.php
@@ -9,15 +9,29 @@ include ('header.php');
 include('dbconnect.php');
 
 $query = $conn->query("SELECT room_name, room_size, room_desc FROM room");
-$row=$query->fetch_array();
+//$row=$query->fetch_array();
 $count = mysql_num_rows($query);
 
 
+/* ///TABLE///
 echo "<table>";
 
 while($row <= $count){
     echo "<tr><td>" . $row['room_name'] . "</td><td>" . $row['room_size'] . "</td><td>". $row['room_desc'] . "</td></tr>";
 }
 echo "</table>";
+*/
+
+
+if ($query->num_rows > 0) {
+    // output data of each row
+    while($row = $query->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+
 $conn->close();
 ?>

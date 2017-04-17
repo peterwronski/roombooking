ed4e31c28d8c90241a14c3b5a1c983da3e8f9d03
peterwronski <1408463@rgu.ac.uk>
2017-04-17 01:35:31 +0100

    rooms

--- rooms.php
+++ rooms.php
@@ -26,11 +26,8 @@ echo '
 <div class="container" id="rooms">
     <div class="row"><h1>Our Rooms @ RGU</h1>
         <div class="col-lg-8 col-lg-offset-2 contentbox">
-<table>
+<table>';
 
-
-
-';
 if ($query->num_rows > 0) {
     // output data of each row
     while($row = $query->fetch_assoc()) {

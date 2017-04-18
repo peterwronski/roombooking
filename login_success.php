ce492c59b74c2bf5503bd4ee5d04269b3044816a
peterwronski <1408463@rgu.ac.uk>
2017-04-18 20:22:56 +0100

    addbooking

--- login_success.php
+++ login_success.php
@@ -2,8 +2,8 @@
 
 include('header.php');
 if (isset($_SESSION['userloggedin'])&&(($_SESSION['userloggedin']) == true)){
-echo $_SESSION['studentid'];
-    //header("Location:index.php");
+//echo $_SESSION['studentid'];
+    header("Location:index.php");
 }
 
 else {

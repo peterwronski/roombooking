79cb13a7f7a57b0d96d4826952114155efeff295
peterwronski <1408463@rgu.ac.uk>
2017-04-18 22:58:26 +0100

    addbooking

--- addbooking.php
+++ addbooking.php
@@ -28,13 +28,11 @@ $studentid = $conn->real_escape_string($studentid);
 
 $query_selectall = "SELECT * FROM booking WHERE room_id= '$roomid' AND bookdate = '$bookingdate' AND booktime='$bookingtime'";
 $sql=$conn->query($query_selectall);
-
-
-
 $count = $sql->num_rows;
 
+
 if($count==0){
-    $query_insertbooking="INSERT INTO booking VALUES ('$studentid','$roomid','$bookingdate','$bookingtime','0' ";
+    $query_insertbooking="INSERT INTO booking (student_id, room_id, bookdate, booktime, booking_status) VALUES ('$studentid','$roomid','$bookingdate','$bookingtime','0' ";
 
     if ($conn->query($query_insertbooking) === TRUE) {
         echo 'Booking Added!';

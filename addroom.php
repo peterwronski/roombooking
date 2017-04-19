c2a4f71b68cec8b96dd284849fcd5ba262802901
peterwronski <1408463@rgu.ac.uk>
2017-04-20 00:59:06 +0100

    updating links

--- addroom.php
+++ addroom.php
@@ -8,7 +8,7 @@ echo'<div class="container" id="#addroom">
 
 if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin'){
     echo'
-    <form action="addroom.php" method="POST">
+    <form action="addroomscr.php" method="POST">
                    <label><b>Room ID</b></label>
                    <input type="text" name="roomid" placeholder="Room ID e.g. RM11" id="roomname" maxlength="4" required/>
 <br/>            
@@ -28,28 +28,11 @@ if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin'){
         </div>
     </div>
 </div>    ';
-};
+}
 
-if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin') {
-    $roomid = $_POST["roomid"];
-    $roomname = $_POST["roomname"];
-    $roomsize = $_POST["roomsize"];
-    $roomdesc = $_POST["roomdesc"];
-
-    $roomid = $conn->real_escape_string($roomid);
-    $roomname = $conn->real_escape_string($roomname);
-    $roomsize = $conn->real_escape_string($roomsize);
-    $roomdesc = $conn->real_escape_string($roomdesc);
-
-    $query = "INSERT INTO room (room_id, room_size, room_desc, room_name) VALUES ('$roomid','$roomsize','$roomdesc','$roomname')";
+else{
+    echo'<h1> You ain\'t supposed to be here';
+}
 
 
-    if ($conn->query($query) === TRUE) {
-        header("Location:index.php#rooms");
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-};
-$conn->close();
 ?>
---
+++ addroomscr.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CG
+ * Date: 20/04/2017
+ * Time: 00:57
+ */
+if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin') {
+    $roomid = $_POST["roomid"];
+    $roomname = $_POST["roomname"];
+    $roomsize = $_POST["roomsize"];
+    $roomdesc = $_POST["roomdesc"];
+
+    $roomid = $conn->real_escape_string($roomid);
+    $roomname = $conn->real_escape_string($roomname);
+    $roomsize = $conn->real_escape_string($roomsize);
+    $roomdesc = $conn->real_escape_string($roomdesc);
+
+    $query = "INSERT INTO room (room_id, room_size, room_desc, room_name) VALUES ('$roomid','$roomsize','$roomdesc','$roomname')";
+
+
+    if ($conn->query($query) === TRUE) {
+        header("Location:index.php#rooms");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+};
+$conn->close();

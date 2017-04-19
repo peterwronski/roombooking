d0702414baa5a94484307401db916f0ca55b40fd
peterwronski <1408463@rgu.ac.uk>
2017-04-19 22:56:43 +0100

    checkbooking

--- checkbooking.php
+++ checkbooking.php
@@ -62,6 +62,24 @@ if ($query->num_rows > 0) {
             "</td><td class=\"rooms\">". $row['bookdate'] .
             "</td><td class=\"rooms\">". $row['spec_req'] .
             "</td><td class=\"rooms\">" .$_SESSION['bookingstatus'] .'</td></tr>';
+
+        switch ($row['booking_status']){
+            case '0':
+                $_SESSION['bookingstatus']='Awaiting Response';
+                break;
+            case '1':
+                $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-ok"></span>APPROVED</p> ';
+                break;
+            case '2':
+                $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-remove"></span>DENIED</p> ';
+                break;
+            default:
+                $_SESSION['bookingstatus']= 'Looks like something is wrong with your booking.';
+                break;
+
+        };
+
+
     }while($row=$query->fetch_array());
 } else {
     echo "No bookings to show at the moment";

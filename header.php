6c18f732ce4fea2175675ff48b820f3f2a455aee
peterwronski <1408463@rgu.ac.uk>
2017-04-20 00:52:00 +0100

    updating links

--- header.php
+++ header.php
@@ -6,8 +6,15 @@
     <title>BookARoom@RGU</title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="style.css">
+    <script type="text/javascript"></script>
+    <script>
+        $(document).ready(function(){
+            $('.dropdown-toggle').dropdown()
+        });
+    </script>
 
 
 </head>

de4a840636b72f6beb30a539159e09ca5893c594
peterwronski <1408463@rgu.ac.uk>
2017-04-20 13:45:28 +0100

    rooms

--- checkbooking.php
+++ checkbooking.php
@@ -14,21 +14,7 @@ $row=$query->fetch_assoc();
 
 
 
-switch ($row){
-    case '0':
-        $_SESSION['bookingstatus']="Awaiting Response";
-        break;
-    case '1':
-        $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-ok"></span>APPROVED</p> ';
-        break;
-    case '2':
-        $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-remove"></span>DENIED</p> ';
-        break;
-    default:
-        $_SESSION['bookingstatus']= 'Looks like something is wrong with your booking.';
-        break;
 
-};
 
 
 
@@ -59,7 +45,21 @@ if ($query->num_rows > 0) {
 $i=0;
     while($query->num_rows>$i) {
 
+        switch ($row){
+            case '0':
+                $_SESSION['bookingstatus']="Awaiting Response";
+                break;
+            case '1':
+                $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-ok"></span>APPROVED</p> ';
+                break;
+            case '2':
+                $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-remove"></span>DENIED</p> ';
+                break;
+            default:
+                $_SESSION['bookingstatus']= 'Looks like something is wrong with your booking.';
+                break;
 
+        };
 
         echo "<tr><td  class=\"rooms\">" . $row['student_id'] .
              "</td><td class=\"rooms\">" . $row['room_id'] .

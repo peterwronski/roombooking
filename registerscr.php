a8383644e7227069f23937eaee92784f32a6b8c0
peterwronski <1408463@rgu.ac.uk>
2017-04-20 09:28:01 +0100

    updating links

--- registerscr.php
+++ registerscr.php
@@ -36,8 +36,8 @@ $studentid = $conn->real_escape_string($studentid);
 $name = $conn->real_escape_string($name);
 $password = $conn->real_escape_string($password);
 
-$hashAndSalt = password_hash($password, PASSWORD_BCRYPT);
-echo $hashAndSalt;
+$hashAndSalt = password_hash($password, PASSWORD_DEFAULT);
+var_dump($hashed_password);
 /*$check_studentid = $conn->query("SELECT student_id FROM user WHERE student_id='$studentid'");
 $count=$check_studentid->num_rows;
 

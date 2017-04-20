00bfdf654725235239248f057eb82a9272a324de
peterwronski <1408463@rgu.ac.uk>
2017-04-20 11:33:03 +0100

    updating links

--- addroom.php
+++ addroom.php
@@ -10,18 +10,18 @@ if (isset($_SESSION['userloggedin'])&&$_SESSION['userloggedin'] == 'sysAdmin'){
     echo'
     <form action="addroomscr.php" method="POST">
                    <label><b>Room ID</b></label>
-                   <input type="text" name="roomid" placeholder="Room ID e.g. RM11" id="roomname" maxlength="4" required/>
+                   <input type="text" name="roomid" placeholder="Room ID e.g. RM11" maxlength="4" required/>
 <br/>            
                    <label><b>Room Name</b></label>
-                   <input type="text" name="roomname" placeholder="Room Name" id="roomname" required/>
+                   <input type="text" name="roomname" placeholder="Room Name" required/>
 <br/>
                    <label><b>Room Size</b></label>
-                   <input type="text" name="roomsize" id="roomsize" placeholder="Room size" required/>
+                   <input type="text" name="roomsize" placeholder="Room size" required/>
 <br/>
                    <label><b>Room Description</b></label>
-                   <textarea name="roomdesc" id="roomdesc" placeholder="Enter a short description of the room, i.e. number of desks, computers, etc."></textarea>
+                   <textarea name="roomdesc"  placeholder="Enter a short description of the room, i.e. number of desks, computers, etc."></textarea>
                    
-                   <input type="submit" name="addroom-submit"/>
+                   <input type="submit"/>
 
                </form>
  

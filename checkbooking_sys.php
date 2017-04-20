8dc06dfc66f39b42845ba57aca7b31819d141829
peterwronski <1408463@rgu.ac.uk>
2017-04-20 01:21:45 +0100

    updating links

--- checkbooking_sys.php
+++ checkbooking_sys.php
@@ -8,8 +8,8 @@
 session_start();
 include('dbconnect.php');
 
-$studentid = $_SESSION['studentid'];
-$query = $conn->query("SELECT booking.student_id, booking.room_id, booking.bookdate, booking.booktime, booking.booking_status, booking.spec_req, room.room_name FROM booking, room WHERE booking.room_id = room.room_id");
+
+$query = $conn->query("SELECT booking.book_id, booking.student_id, booking.room_id, booking.bookdate, booking.booktime, booking.booking_status, booking.spec_req, room.room_name FROM booking, room WHERE booking.room_id = room.room_id");
 //$row=$query->fetch_array();
 
 
@@ -21,7 +21,8 @@ echo '
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 contentbox">
 <table class="rooms">
-    <tr> <th  class="rooms">Student ID</th>
+    <tr> <th  class="rooms">Booking ID</th>
+        <th  class="rooms">Student ID</th>
          <th  class="rooms">Room ID</th>
          <th  class="rooms">Room Name</th>
          <th  class="rooms">Booking Time</th>
@@ -35,23 +36,8 @@ if ($query->num_rows > 0) {
     // output data of each row
     while($row=$query->fetch_assoc()) {
 
-        switch ($row){
-            case '0':
-                $_SESSION['bookingstatus']='Awaiting Response';
-                break;
-            case '1':
-                $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-ok"></span>APPROVED</p> ';
-                break;
-            case '2':
-                $_SESSION['bookingstatus']='<p><span class="glyphicon glyphicon-remove"></span>DENIED</p> ';
-                break;
-            default:
-                $_SESSION['bookingstatus']= 'Looks like something is wrong with your booking.';
-                break;
-
-        };
-
-        echo "<tr><td  class=\"rooms\">" . $row['student_id'] .
+        echo "<tr><td  class=\"rooms\">" . $row['book_id'] .
+            "<td  class=\"rooms\">" . $row['student_id'] .
             "</td><td class=\"rooms\">" . $row['room_id'] .
             "</td><td class=\"rooms\">". $row['room_name'] .
             "</td><td class=\"rooms\">". $row['booktime'] .
@@ -60,9 +46,9 @@ if ($query->num_rows > 0) {
             '</td><td class=\"rooms\"> <form method="POST"><select id="bookingstatusadmin"> <option value="0">Awaiting Approval</option>
                                                                    <option value="1">Approve</option>
                                                                    <option value="2">Deny</option></select></form></td></tr>';
-        $studentidadmin=$row['student_id'];
+        $bookid=$row['book_id'];
         $bookingstatusadmin=$_POST['bookingstatusadmin'];
-        $updatestatus="UPDATE booking SET booking_status='$bookingstatusadmin' WHERE student_id='$studentid'";
+        $updatestatus="UPDATE booking SET booking_status='$bookingstatusadmin' WHERE book_id='$bookid'";
         $conn->query($updatestatus);
     };
 

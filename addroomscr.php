89cf74e41a2f1c28aabdadb7d253463719f72be4
peterwronski <1408463@rgu.ac.uk>
2017-04-21 14:11:06 +0100

    updating links

--- addroomscr.php
+++ addroomscr.php
@@ -18,20 +18,22 @@ include('dbconnect.php');
     $roomsize = $conn->real_escape_string($roomsize);
     $roomdesc = $conn->real_escape_string($roomdesc);
 
+    $checkid_query = "SELECT * FROM room WHERE room_id = '$roomid'";
+    $sql=$conn->query($checkid_query);
+    $count = $sql->num_rows;
+
     $query = "INSERT INTO room (room_id, room_size, room_desc, room_name) VALUES ('$roomid','$roomsize','$roomdesc','$roomname')";
 
 //$sql=$conn->query($query_selectall);
 
-
-    if ($conn->query($query) === TRUE) {
-        echo '<script type="text/javascript">
+if($count == 0){
+    $conn->query($query);
+    echo '<script type="text/javascript">
     alert("Room created.");
     window.location = "addroom.php"
               </script>';
     }
     else{
-
-
    echo '<script type="text/javascript">
     alert("Room ID already taken. Try a different one."); 
     window.location = "addroom.php" ';

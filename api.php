95d918939db14f12e6dadfa0b842e221a8a18ce4
peterwronski <1408463@rgu.ac.uk>
2017-05-05 00:35:46 +0100

    API

--- api.php
+++ api.php
@@ -12,14 +12,14 @@ function getRoomByID($id){
 
     $roomInfo = array();
     global $conn;
-    $getRoomByID_query = $conn -> mysqli_query ("SELECT room_id, room_name, room_size, room_desc FROM room WHERE room_id = $id' ");
+    $getRoomByID_query = $conn -> query ("SELECT room_id, room_name, room_size, room_desc FROM room WHERE room_id = $id' ");
 
 
-    while($row = $getRoomByID_query -> mysqli_fetch_assoc()){
+    while($row = $getRoomByID_query -> fetch_assoc()){
         $roomInfo = $row; // Inside while loop
     };
 
-   var_dump($roomInfo);
+   echo ($roomInfo);
     /*switch ($id) {
         case 1:
             $roomInfo = array("room_id" => "RM01", "room_name" => "RoomName1", "room_size" => "5", "room_desc" => "ROOMDESCRIPTIONROOMDESCRIPTION");

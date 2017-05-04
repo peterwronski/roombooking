6c8258a8968145ad1d63b38b5d3faee5b99506c3
peterwronski <1408463@rgu.ac.uk>
2017-05-04 23:52:04 +0100

    API

--- api.php
+++ api.php
@@ -14,8 +14,8 @@ function getRoomByID($id){
     $getRoomByID_query = 'SELECT room_id, room_name, room_size, room_desc FROM room WHERE room_id = "$id" ';
 
     while($row = mysqli_fetch_assoc( $getRoomByID_query)){
-        $roomInfo[] = $row; // Inside while loop
-    }
+        $roomInfo = $row; // Inside while loop
+    };
 
     return $roomInfo;
     /*switch ($id) {

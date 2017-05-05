0fd907455ca202b2ad284d1b92bc5c52e3b9ac61
peterwronski <1408463@rgu.ac.uk>
2017-05-05 02:07:12 +0100

    API

--- viewroom.php
+++ viewroom.php
@@ -38,7 +38,7 @@ else
 
     foreach ($roomList as $room){
              echo'<li>
-                <a href=http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomByID&id=' . $room["room_id"]  .'alt= \"Room \" '. $room["room_id"] .'>' .$room["room_name"] .'</a>
+                <a href=http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomByID&id=' .$room["room_id"]  .'>' .$room["room_id"] .', ' .$room["room_name"] .'</a>
             </li> ';
         };
         echo'</ul>';

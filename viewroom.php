9aa951741b4d547120561745ebb8f3d9e1309535
peterwronski <1408463@rgu.ac.uk>
2017-05-05 03:19:38 +0100

    API

--- viewroom.php
+++ viewroom.php
@@ -3,47 +3,63 @@
  <body>
 
 <?php
-if (isset($_GET["action"]) && isset($_GET["id"]) && $_GET["action"] == "getRoomByID")
-{
-    $roomInfo = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomByID&id=' . $_GET["id"]);
-    $roomInfo = json_decode($roomInfo, true);
-    ?>
-    <table>
-        <tr>
-            <td>Room ID: </td><td> <?php echo $roomInfo["room_id"] ?></td>
-        </tr>
+    if (isset($_GET["action"]) && isset($_GET["id"])) {
+        switch ($_GET["action"]) {
+            case "getRoomByID":
 
-        <tr>
-            <td>Room Name: </td><td> <?php echo $roomInfo["room_name"] ?></td>
-        </tr>
-        <tr>
-            <td>Room Size </td><td> <?php echo $roomInfo["room_size"] ?></td>
-        </tr>
-        <tr>
-            <td>Desc </td><td> <?php echo $roomInfo["room_desc"] ?></td>
-        </tr>
-    </table>
-    <br />
-    <a href="http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomList" alt="Room List">Return to the room list</a>
+                $roomInfo = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomByID&id=' . $_GET["id"]);
+                $roomInfo = json_decode($roomInfo, true);
+                ?>
+                <table>
+                    <tr>
+                        <td>Room ID:</td>
+                        <td> <?php echo $roomInfo["room_id"] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Room Name:</td>
+                        <td> <?php echo $roomInfo["room_name"] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Room Size</td>
+                        <td> <?php echo $roomInfo["room_size"] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Desc</td>
+                        <td> <?php echo $roomInfo["room_desc"] ?></td>
+                    </tr>
+                </table>
+                <br/>
+                <a href="http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomList" alt="Room List">Return to
+                    the room list</a>
+                <?php
 
-    <?php
-}
-//if(isset($_GET["action"]) && isset($_GET["id"]) && $_GET["action"] == "getRoomList")
-else {
-    $roomList = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomList');
-    $roomList = json_decode($roomList, true);
+                break;
 
-    echo '<ul>';
 
-    foreach ($roomList as $room) {
-          //echo'<li>' .$room["room_name"] .'</li>';
-            echo' <li>   <a href=http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomByID&id=' .$room["room_id"]  .'>' .$room["room_id"] .', ' .$room["room_name"] .'</a>
+
+
+            case "getRoomList":
+
+                $roomList = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomList');
+                $roomList = json_decode($roomList, true);
+
+                echo '<ul>';
+
+                foreach ($roomList as $room) {
+                    echo ' <li>   <a href=http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomByID&id=' . $room["room_id"] . '>' . $room["room_id"] . ', ' . $room["room_name"] . '</a>
             </li> ';
-        };
-        echo'</ul>';
+                };
+                echo '</ul>';
 
-    };
+                break;
 
+            default:
+
+                echo 'This function name doesn\'t exist';
+                break;
+        }
+
+}
 
 ?>
 </body>

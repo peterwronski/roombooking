7f1b499878d78923d406d2565659f8d8ae0211d9
peterwronski <1408463@rgu.ac.uk>
2017-05-05 03:26:52 +0100

    API

--- viewroom.php
+++ viewroom.php
@@ -3,63 +3,48 @@
  <body>
 
 <?php
-    if (isset($_GET["action"]) && isset($_GET["id"])) {
-        switch ($_GET["action"]) {
-            case "getRoomByID":
+if (isset($_GET["action"]) && isset($_GET["id"]) && $_GET["action"] == "getRoomByID")
+{
+    $roomInfo = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomByID&id=' . $_GET["id"]);
+    $roomInfo = json_decode($roomInfo, true);
+    ?>
+    <table>
+        <tr>
+            <td>Room ID: </td><td> <?php echo $roomInfo["room_id"] ?></td>
+        </tr>
 
-                $roomInfo = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomByID&id=' . $_GET["id"]);
-                $roomInfo = json_decode($roomInfo, true);
-                ?>
-                <table>
-                    <tr>
-                        <td>Room ID:</td>
-                        <td> <?php echo $roomInfo["room_id"] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Room Name:</td>
-                        <td> <?php echo $roomInfo["room_name"] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Room Size</td>
-                        <td> <?php echo $roomInfo["room_size"] ?></td>
-                    </tr>
-                    <tr>
-                        <td>Desc</td>
-                        <td> <?php echo $roomInfo["room_desc"] ?></td>
-                    </tr>
-                </table>
-                <br/>
-                <a href="http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomList" alt="Room List">Return to
-                    the room list</a>
-                <?php
+        <tr>
+            <td>Room Name: </td><td> <?php echo $roomInfo["room_name"] ?></td>
+        </tr>
+        <tr>
+            <td>Room Size </td><td> <?php echo $roomInfo["room_size"] ?></td>
+        </tr>
+        <tr>
+            <td>Desc </td><td> <?php echo $roomInfo["room_desc"] ?></td>
+        </tr>
+    </table>
+    <br />
+    <a href="http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomList" alt="Room List">Return to the room list</a>
 
-                break;
+    <?php
+}
+elseif (isset($_GET["action"]) && isset($_GET["id"]) && $_GET["action"] == "getRoomList") {
+    $roomList = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomList');
+    $roomList = json_decode($roomList, true);
 
+    echo '<ul>';
 
-
-
-            case "getRoomList":
-
-                $roomList = file_get_contents('http://bookaroom.azurewebsites.net/api.php?action=getRoomList');
-                $roomList = json_decode($roomList, true);
-
-                echo '<ul>';
-
-                foreach ($roomList as $room) {
-                    echo ' <li>   <a href=http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomByID&id=' . $room["room_id"] . '>' . $room["room_id"] . ', ' . $room["room_name"] . '</a>
+    foreach ($roomList as $room) {
+        //echo'<li>' .$room["room_name"] .'</li>';
+        echo '    <a href=http://bookaroom.azurewebsites.net/viewroom.php?action=getRoomByID&id=' . $room["room_id"] . '>' . $room["room_id"] . ' - ' . $room["room_name"] . '</a>
             </li> ';
-                };
-                echo '</ul>';
-
-                break;
-
-            default:
-
-                echo 'This function name doesn\'t exist';
-                break;
-        }
+    };
+    echo '</ul>';
 
 }
+
+else { echo 'There is no function with that name';}
+
 
 ?>
 </body>
